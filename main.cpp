f69bf32bf9a839c4ea5484ad1459d0034f07f80d
Mike Gu <gyz0072004@hotmail.com>
2015-09-19 14:58:27 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  math
+//
+//  Created by 顾逸之 on 15/9/19.
+//  Copyright © 2015年 顾逸之. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    char* a;
+    if (a==0) {
+        cout<<"hello world!\n";
+    }
+    return 0;
+}
